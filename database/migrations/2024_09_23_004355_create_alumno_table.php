28cef542c5818622a808a530aa1b2e37f126be1f
dogpool22 <david.22.ahuite@gmail.com>
2024-11-19 17:02:43 -0500

    add: migracion correo no unico

--- database/migrations/2024_09_23_004355_create_alumno_table.php
+++ database/migrations/2024_09_23_004355_create_alumno_table.php
@@ -18,7 +18,7 @@ class CreateAlumnoTable extends Migration
             $table->string('nombre', 255); // Columna para el nombre del alumno
             $table->string('apellido', 255); // Columna para el apellido del alumno
             $table->string('dni', 15); // Columna para el DNI (máximo 15 caracteres, sin unique)
-            $table->string('correo', 255)->unique(); // Columna para el correo (debe ser único)
+            $table->string('correo', 255); // Columna para el correo (debe ser único)
             $table->unsignedBigInteger('idcurso'); // Relación con la tabla 'curso'
             $table->foreign('idcurso')->references('idcurso')->on('curso')->onDelete('restrict')->onUpdate('restrict'); // Llave foránea con restricciones
             $table->boolean('estado')->default(1); // Columna para el estado (activo/inactivo)
