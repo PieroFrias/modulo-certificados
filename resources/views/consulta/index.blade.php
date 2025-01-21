9f9f71de479fd8e8219c340fc1e178ba3bf8db76
PieroFrias <piefrimo@gmail.com>
2025-01-21 18:00:37 -0500

    fix: details Ui

--- resources/views/consulta/index.blade.php
+++ resources/views/consulta/index.blade.php
@@ -46,8 +46,6 @@
             top: 0;
             left: 0;
             width: 100%;
-            background-color: rgba(0, 0, 0, 0.7);
-            padding: 10px 20px;
             z-index: 1000;
         }
 
@@ -59,10 +57,9 @@
             color: white;
         }
 
-        .form-section {
+        /* .form-section {
             padding: 50px 20px;
-            background-color: #f4f4f4;
-        }
+        } */
 
         #title-banner{
             font-family: "Play", serif;
@@ -109,22 +106,17 @@
 
     </style>
 
-    <header class="navbar text-white bg-white/10 shadow-lg shadow-black/40 py-6">
+    <header class="navbar text-white bg-black/90 shadow-lg shadow-black/40 py-6">
         <div class="container mx-auto flex  items-center justify-between max-w-screen-xl  flex-wrap p-4">
             <!-- Logo y título -->
             <div class="flex items-center space-x-4">
                 <img src="https://aguasamazonicas.org/wp-content/uploads/2021/06/imagem_2023-05-29_120804614.png" alt="Logo" class="h-16 w-16 rounded-md">
-                <h1 class="text-sm md:text-3xl font-bold">Consulta de Certificados IIAP</h1>
+                <h1 class="text-xs sm:text-sm md:text-3xl font-bold">Consulta de Certificados IIAP</h1>
             </div>
 
             <!-- Botón de menú hamburguesa -->
-            <!-- <button id="menu-toggle" class="lg:hidden flex items-center focus:outline-none">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 text-white">
-                    <path fill-rule="evenodd" d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd" />
-                </svg>
-            </button> -->
+
             <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-                <span class="sr-only">Open main menu</span>
                 <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
                 </svg>
@@ -153,22 +145,32 @@
     <!-- Contenido principal -->
     <div  class="banner flex flex-col gap-8 p-6 justify-center items-start">
         <div class=" flex flex-col">
-            <h1 id="title-banner" class="text-5xl  text-white">
+            <h1 id="title-banner" class="text-4xl sm:text-5xl  text-white">
                 TODOS TUS CERTIFICADOS
             </h1>
             <p id="footer-banner" class="text-lg text-white">EN UN SOLO LUGAR</p>
         </div>
-        <button id="footer-banner" class="bg-[#EB8021] rounded-lg p-4 text-sm text-white scale-100 hover:scale-105">¡CONSULTAR AQUI!</button>
+        <button 
+            id="footer-banner"
+            onclick="document.getElementById('form-section').scrollIntoView({behavior: 'smooth'})"
+            class="bg-[#EB8021] rounded-lg p-4 text-sm text-white scale-100 hover:scale-105"
+        >
+            ¡CONSULTAR AQUI!
+        </button>
     </div>
-    <main class="form-section flex flex-col gap-4 w-full">
+    <main id="form-section" class="form-section flex flex-col lg:flex-row items-start gap-4 p-8 w-full">
         <!-- Form Section -->
-            <section class="flex flex-col w-96 bg-red-100 p-4 gap-4 rounded shadow">
-                <h1 class="text-lg font-bold">Formulario de búsqueda</h1>
-                <form action="{{ route('consulta.index') }}" method="GET" class="flex flex-col gap-6">
+        <section class="flex flex-col w-full lg:max-w-md p-4 gap-4 rounded-lg shadow-lg mx-auto sm:my-8">
+            <h1 class="text-lg font-bold text-center sm:text-left">Formulario de búsqueda</h1>
+            <form action="{{ route('consulta.index') }}" method="GET" class="flex flex-col gap-6">
                 <!-- Selector de tipo de búsqueda -->
                 <div>
                     <label for="search-type" class="block text-sm font-medium text-gray-700">Seleccione el tipo de búsqueda</label>
-                    <select id="search-type" onchange="toggleSearchInputs()" class="w-full px-4 py-2 mt-2 text-sm border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none">
+                    <select 
+                        id="search-type" 
+                        onchange="toggleSearchInputs()" 
+                        class="w-full px-4 py-2 mt-2 text-sm border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none"
+                    >
                         <option value="correo" selected>Buscar por Correo</option>
                         <option value="dni">Buscar por DNI</option>
                     </select>
@@ -177,36 +179,55 @@
                 <!-- Campo de entrada para DNI -->
                 <div id="dni-input-container" class="hidden">
                     <label for="dni-input" class="block text-sm font-medium text-gray-700">Ingrese su DNI</label>
-                    <input id="dni-input" type="number" name="dni" value="{{ request('dni') }}" placeholder="Ingrese su DNI"
-                        class="w-full px-4 py-2 mt-2 text-sm border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none placeholder-gray-400">
+                    <input 
+                        id="dni-input" 
+                        type="number" 
+                        name="dni" 
+                        value="{{ request('dni') }}" 
+                        placeholder="Ingrese su DNI"
+                        class="w-full px-4 py-2 mt-2 text-sm border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:outline-none placeholder-gray-400"
+                    >
                 </div>
 
                 <!-- Campo de entrada para Correo -->
                 <div id="correo-input-container">
                     <label for="correo-input" class="block text-sm font-medium text-gray-700">Ingrese su Correo</label>
-                    <input id="correo-input" type="email" name="correo" value="{{ request('correo') }}" placeholder="Ingrese su correo"
-                        class="w-full px-4 py-2 mt-2 text-sm border border-gray-300 rounded-lg focus:ring-2 focus:ring-green-500 focus:outline-none placeholder-gray-400">
+                    <input 
+                        id="correo-input" 
+                        type="email" 
+                        name="correo" 
+                        value="{{ request('correo') }}" 
+                        placeholder="Ingrese su correo"
+                        class="w-full px-4 py-2 mt-2 text-sm border border-gray-300 rounded-lg focus:ring-2 focus:ring-green-500 focus:outline-none placeholder-gray-400"
+                    >
                 </div>
 
                 <!-- Contenedor para botones -->
-                <div class="flex justify-center gap-4">
+                <div class="flex flex-col sm:flex-row justify-center md:justify-end p-4 gap-4">
                     <!-- Botón Limpiar -->
-                    <button type="button" onclick="window.location.href='{{ route('consulta.index') }}'"
-                            class="bg-gray-600 hover:bg-gray-500 text-white py-2 px-6 rounded-lg font-medium text-sm">
+                    <button 
+                        type="button" 
+                        onclick="window.location.href='{{ route('consulta.index') }}'"
+                        class="bg-gray-600 hover:bg-gray-500 text-white py-2 px-6 rounded-lg font-medium text-sm w-full sm:w-auto"
+                    >
                         Limpiar
                     </button>
                     <!-- Botón Buscar -->
-                    <button type="submit" class="bg-green-600 hover:bg-green-500 text-white py-2 px-6 rounded-lg font-medium text-sm">
+                    <button 
+                        type="submit"
+                        class="bg-green-600 hover:bg-green-500 text-white py-2 px-6 rounded-lg font-medium text-sm w-full sm:w-auto"
+                    >
                         Buscar
                     </button>
                 </div>
             </form>
-            </section>
-            <section class="flex flex-col gap-4 w-full bg-red-100 p-4 rounded shadow">
+        </section>
+
+            <section class="flex flex-col gap-4 w-full  p-4 rounded shadow">
                 <h1 class="text-lg font-bold">Tus certificados</h1>
                 @if ($alumnos && $alumnos->isNotEmpty())
-                    <div class="overflow-x-auto bg-white bg-opacity-90 p-6 rounded-lg shadow">
-                        <table class="table-auto w-full bg-white shadow-lg rounded-lg border-collapse">
+                    <div class="overflow-x-auto bg-opacity-90 p-6">
+                        <table class="table-auto w-full bg-white rounded-lg border-collapse">
                             <thead class="bg-gray-200">
                                 <tr>
                                     <th class="px-6 py-4 text-left text-sm font-bold text-gray-700">Nombre</th>
@@ -227,6 +248,7 @@
                                             <div class="flex gap-2">
                                                 @if ($alumno->codigo)
                                                     <a href="{{ route('consulta.descargarCertificado', $alumno->codigo) }}"
+                                        
                                                     class="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 text-center">Descargar</a>
                                                 @else
                                                     <span class="text-red-500">No disponible</span>
